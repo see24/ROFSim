751c1144bb70a334177c952bb4186315b839be27
Valentin <valentin.lucet@gmail.com>
2020-12-17 15:32:54 -0500

    attempt update

--- process_spades_outputs.R
+++ process_spades_outputs.R
@@ -1,2 +1,6 @@
 ### Primary Transformer
 print("Primary Transformer: processing spades outputs")
+# TODO:
+# - Access spade output data file at the datasheet ROFSim_InputSpadesObject
+# - Validate format of file
+# - Extract info and process them for the next transformer
